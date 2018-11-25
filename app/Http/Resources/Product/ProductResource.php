5c3356e81c6ba9d15ec343c3642dd9dca205a35f
unknown <almeshari93@gmail.com>
2018-11-26 00:44:02 +0300

    Product Collection Resource

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -19,7 +19,7 @@ class ProductResource extends JsonResource
             'description' => $this->detail,
             'price' => $this->price,
             'stock' => $this->stock == 0 ? 'Out of Stock' : $this->stock,
-            'discount' => $this->descount,
+            'discount' => $this->discount,
             'totalPrice' =>  round(( 1 - ($this->discount/100)) * $this->price, 2),
             'rating' => $this->reviews->count() > 0 ?  round($this->reviews->sum('starRating')/$this->reviews->count(),2) : 'No Rating Yet',
             'reviewsNumber' => $this->reviews->count() > 0 ? $this->reviews->count() : 'No Rating Yet',
